7d6e26b2ee3b34884244f6b9821c8bd7b1e8cac5
rohit solunke <rohitsolunke99@gmail.com>
2023-11-27 13:13:46 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+from test.support import requires_linux_version
+
+HYPEN_E_DOT='-e .'
+def get_requirments(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirments
+    '''
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments=file_obj.readlines()
+        requirments=[req.replace("\n","")for req in requirments]
+
+        if HYPEN_E_DOT in requirments:
+            requirments.remove(HYPEN_E_DOT)
+
+    return requirments
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='rohit',
+author_email='rohitsolunke99@gmail.com',
+packages=find_packages(),
+install_requires = get_requirments('requirments.txt')
+)
+
+
